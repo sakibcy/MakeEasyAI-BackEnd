308e1d96a3d35eed61b7bb409d839784a33c8f30
Md Sakibul Hasan <sakibal623@gmail.com>
2024-04-20 12:20:36 +0600

    removed Prisma ORM.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
 import express, {Express, Request, Response} from "express";
 import dotenv from "dotenv";
-import { PrismaClient } from './generated/client'
 
-const prisma = new PrismaClient()
-
-import db from "./startup/db";
 dotenv.config();
-
 
 const app: Express = express();
 
 require('./startup/routes')(app);
-db()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
