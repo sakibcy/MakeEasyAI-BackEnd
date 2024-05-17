60857bfacec8bc5ce55f20f564c15fbba2582da5
Md Sakibul Hasan <sakibal623@gmail.com>
2024-05-17 18:20:35 +0600

    Created mail and pass validator for login and singup.

--- dist/controllers/authController.js
+++ dist/controllers/authController.js
@@ -2,68 +2,117 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.login_post = exports.signup_post = void 0;
 const client_1 = require("../../node_modules/.prisma/client");
+const validator = require("validator");
 const prisma = new client_1.PrismaClient();
 const signup_post = async (req, res) => {
-    const data = {
-        email: req.body.email,
-        password: req.body.password
-    };
-    const user = await prisma.users.findUnique({
-        where: {
-            email: req.body.email,
-        }
-    });
-    if (user) {
-        res.json({
+    const isEmail = validator.isEmail(req.body.email);
+    const isLength = validator.isLength(req.body.password, { min: 6, max: undefined });
+    if (!isEmail) {
+        res.status(400).json({
             "status": {
                 error: true,
-                code: 401,
-                type: "Unauthorized",
-                message: "User already exists"
+                code: 400,
+                type: "Bad Request",
+                message: "Email is not valid"
+            }
+        });
+    }
+    else if (!isLength) {
+        res.status(400).json({
+            "status": {
+                error: true,
+                code: 400,
+                type: "Bad Request",
+                message: "Password should be at least 6 characters"
             }
         });
     }
     else {
-        const user = await prisma.users.create({ data });
+        const data = {
+            email: req.body.email,
+            password: req.body.password
+        };
+        const user = await prisma.users.findUnique({
+            where: {
+                email: req.body.email,
+            }
+        });
+        if (user) {
+            res.json({
+                "status": {
+                    error: true,
+                    code: 401,
+                    type: "Unauthorized",
+                    message: "User already exists"
+                }
+            });
+        }
+        else {
+            const user = await prisma.users.create({ data });
+            if (user) {
+                res.status(201).json({
+                    "status": {
+                        error: false,
+                        code: 201,
+                        type: "success",
+                        message: "User created successfully"
+                    }
+                });
+            }
+        }
+    }
+};
+exports.signup_post = signup_post;
+async function login_post(req, res) {
+    const isEmail = validator.isEmail(req.body.email);
+    const isLength = validator.isLength(req.body.password, { min: 6, max: undefined });
+    if (!isEmail) {
+        res.status(400).json({
+            "status": {
+                error: true,
+                code: 400,
+                type: "Bad Request",
+                message: "Email is not valid"
+            }
+        });
+    }
+    else if (!isLength) {
+        res.status(400).json({
+            "status": {
+                error: true,
+                code: 400,
+                type: "Bad Request",
+                message: "Password should be at least 6 characters"
+            }
+        });
+    }
+    else {
+        const user = await prisma.users.findUnique({
+            where: {
+                email: req.body.email,
+                password: req.body.password
+            }
+        });
         if (user) {
             res.json({
                 "status": {
                     error: false,
                     code: 200,
                     type: "success",
-                    message: "User created successfully"
+                    message: "User logged in successfully"
                 }
             });
         }
-    }
-};
-exports.signup_post = signup_post;
-async function login_post(req, res) {
-    const user = await prisma.users.findUnique({
-        where: {
-            email: req.body.email,
-            password: req.body.password
+        else {
+            res.status(401).json({
+                "status": {
+                    error: true,
+                    code: 401,
+                    type: "Unauthorized",
+                    message: "Authentication Failure User not registered or check your Email and Password"
+                }
+            });
         }
-    });
-    if (user) {
-        res.json({
-            "status": {
-                error: false,
-                code: 200,
-                type: "success",
-                message: "User logged in successfully"
-            }
-        });
-    }
-    else {
-        res.json({
-            "status": {
-                error: true,
-                code: 401,
-                type: "Unauthorized",
-                message: "Authentication Failure User not registered or check your Email and Password"
-            }
-        });
     }
 }
 exports.login_post = login_post;
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,68 +1,115 @@
 import { PrismaClient } from "../../node_modules/.prisma/client";
+const validator = require("validator");
 
 const prisma = new PrismaClient();
 
 export const signup_post = async (req: any, res: any) => {
-    const data = {
-        email: req.body.email,
-        password: req.body.password
-    };
+    const isEmail = validator.isEmail(req.body.email);
+    const isLength = validator.isLength(req.body.password, { min: 6, max: undefined });
 
-    const user = await prisma.users.findUnique({
-        where: {
-            email: req.body.email,
-        }
-    });
-
-    if (user) {
-        res.json({
+    if (!isEmail) {
+        res.status(400).json({
             "status": {
                 error: true,
-                code: 401,
-                type: "Unauthorized",
-                message: "User already exists"
+                code: 400,
+                type: "Bad Request",
+                message: "Email is not valid"
+            }
+        });
+    } else if (!isLength) {
+        res.status(400).json({
+            "status": {
+                error: true,
+                code: 400,
+                type: "Bad Request",
+                message: "Password should be at least 6 characters"
             }
         });
     } else {
-        const user = await prisma.users.create({ data });
+        const data = {
+            email: req.body.email,
+            password: req.body.password
+        };
+
+        const user = await prisma.users.findUnique({
+            where: {
+                email: req.body.email,
+            }
+        });
+
+        if (user) {
+            res.json({
+                "status": {
+                    error: true,
+                    code: 401,
+                    type: "Unauthorized",
+                    message: "User already exists"
+                }
+            });
+        } else {
+            const user = await prisma.users.create({ data });
+            if (user) {
+                res.status(201).json({
+                    "status": {
+                        error: false,
+                        code: 201,
+                        type: "success",
+                        message: "User created successfully"
+                    }
+                });
+            }
+        }
+    }
+}
+
+export async function login_post(req: any, res: any) {
+    const isEmail = validator.isEmail(req.body.email);
+    const isLength = validator.isLength(req.body.password, { min: 6, max: undefined });
+
+    if (!isEmail) {
+        res.status(400).json({
+            "status": {
+                error: true,
+                code: 400,
+                type: "Bad Request",
+                message: "Email is not valid"
+            }
+        });
+    } else if (!isLength) {
+        res.status(400).json({
+            "status": {
+                error: true,
+                code: 400,
+                type: "Bad Request",
+                message: "Password should be at least 6 characters"
+            }
+        });
+    } else {
+        const user = await prisma.users.findUnique({
+            where: {
+                email: req.body.email,
+                password: req.body.password
+            }
+        })
+
         if (user) {
             res.json({
                 "status": {
                     error: false,
                     code: 200,
                     type: "success",
-                    message: "User created successfully"
+                    message: "User logged in successfully"
+                }
+            });
+        } else {
+            res.status(401).json({
+                "status": {
+                    error: true,
+                    code: 401,
+                    type: "Unauthorized",
+                    message: "Authentication Failure User not registered or check your Email and Password"
                 }
             });
         }
-    }
-}
-
-export async function login_post(req: any, res: any) {
-    const user = await prisma.users.findUnique({
-        where: {
-            email: req.body.email,
-            password: req.body.password
-        }
-    })
-
-    if (user) {
-        res.json({
-            "status": {
-                error: false,
-                code: 200,
-                type: "success",
-                message: "User logged in successfully"
-            }
-        });
-    } else {
-        res.json({
-            "status": {
-                error: true,
-                code: 401,
-                type: "Unauthorized",
-                message: "Authentication Failure User not registered or check your Email and Password"
-         }
-        });
     }
 }
