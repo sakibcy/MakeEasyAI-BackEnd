e5ea9ddf12694ccceeb746ffbeca60ebf7c53356
Md Sakibul Hasan <sakibal623@gmail.com>
2024-07-08 13:06:41 +0600

    moved jwtTokenName to config.

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const { PORT, CREDENTIALS_G } = process.env;
+const JWT_TOKEN_NAME = 'x-auth-token';
+const SALT_ROUND_DB = 10;
 
-export { PORT, CREDENTIALS_G };
+export { PORT, CREDENTIALS_G, JWT_TOKEN_NAME, SALT_ROUND_DB };
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,11 +4,11 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
 import { generateResponse } from "../utils/generateResponse";
+import { JWT_TOKEN_NAME, SALT_ROUND_DB } from "../config";
 
 const prisma = new PrismaClient();
 
 const maxAge = 3 * 24 * 60 * 60; // 3 days
-const tokenName = 'x-auth-token';
 
 const createToken = (id: any) => {
     return jwt.sign({ id }, 'the secret stored in env', {
@@ -20,7 +20,7 @@ export const signup_post = async (req: Request, res: Response) => {
     const isEmail = validator.isEmail(req.body.email);
     const isLength = validator.isLength(req.body.password, { min: 6, max: undefined });
 
-    const salt = await bcrypt.genSalt();
+    const salt = await bcrypt.genSalt(SALT_ROUND_DB);
 
     if (!isEmail) {
         res.status(400).json(generateResponse(
@@ -52,7 +52,7 @@ export const signup_post = async (req: Request, res: Response) => {
             const user = await prisma.users.create({ data });
             if (user) {
                 const token = createToken(user.id);
-                res.setHeader(tokenName, token).cookie(tokenName, token, { httpOnly: true, maxAge: maxAge * 1000 });
+                res.setHeader(JWT_TOKEN_NAME, token).cookie(JWT_TOKEN_NAME, token, { httpOnly: true, maxAge: maxAge * 1000 });
 
                 res.status(201).json(generateResponse(
                     false, 201, "Success", "User created successfully"
@@ -83,7 +83,7 @@ export async function login_post(req: Request, res: Response) {
 
         if (user && bcrypt.compareSync(req.body.password, user.password)) {
             const token = createToken(user.id);
-            res.setHeader(tokenName, token).cookie(tokenName, token, { httpOnly: true, maxAge: maxAge * 1000 });
+            res.setHeader(JWT_TOKEN_NAME, token).cookie(JWT_TOKEN_NAME, token, { httpOnly: true, maxAge: maxAge * 1000 });
 
             res.status(200).json(generateResponse(
                 false, 200, "Success", "User logged in successfully"
