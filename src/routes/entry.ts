7eb3358f35dcb479a0fceecabe09069e3695c4d0
Md Sakibul Hasan <sakibal623@gmail.com>
2024-07-12 09:25:55 +0600

    refactored code.

--- src/routes/entry.ts
+++
@@ -1,9 +0,0 @@
-import express, {Request, Response} from "express";
-
-const router = express.Router();
-
-router.get("/", (req: Request, res: Response) => {
-    res.send("Testing 8:10pm");
-});
-
-export default router;
--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -1,22 +1,20 @@
-import { NextHandleFunction } from "connect";
 import express from "express";
-var cookieParser = require('cookie-parser')
-
 
 import v2_languages from "../routes/v2_languages";
 import v3_languages from "../routes/v3_languages";
 import summarizer from "../routes/summarizer";
 import v3_translate from "../routes/v3_translate";
-import entry from "../routes/entry";
 import authRoutes from "../routes/authRoutes";
-import { requireAuth } from "../middleware/authMiddleware";
 import { API_VERSION } from "../config";
 
 module.exports = function (app: any) {
-    // @ts-ignore
-    app.use(express.json()) // for parsing application/json
-    app.use(express.urlencoded({extended: true})) // for parsing application/x-www-form-urlencoded
-    app.use(cookieParser());
+    const middleware = [
+        express.json(),
+        express.urlencoded({extended: true}),
+        require('cookie-parser')()
+    ]
+
+    app.use(middleware);
 
     app.use(`/${API_VERSION}/languagesv2`, v2_languages);
     app.use(`/${API_VERSION}/languages`, v3_languages);
